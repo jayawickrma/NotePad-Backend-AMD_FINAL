be3959f55446d293ea96d8b550950ed8e4721a50
jayawickrama <nishantharaka2004@gmail.com>
2025-02-27 00:52:12 +0530

    creating Crud Operations

--- NotePad-backend/Controller/PostCOntroller.ts
+++ NotePad-backend/Controller/PostCOntroller.ts
@@ -1,5 +1,14 @@
+import {savePost} from "../Service/PostService";
+
 class PostController{
     async addPost(req:any,resp:any){
+        try{
+            await savePost(req.body);
+            resp.status(201).send("Created...")
+        }catch (err){
+            console.log(err)
+            resp.status(500).send(err)
+        }
 
     }
     async deletePost(req:any,resp:any){
---
+++ NotePad-backend/Service/PostService.ts
@@ -0,0 +1,24 @@
+import {PostDTO} from "../DTO/PostDTO";
+import prisma from "../../prisma/Client";
+
+export async function savePost(postDto:PostDTO){
+    try{
+        const data =await prisma.post.create({
+            data:{
+                title:postDto.title,
+                content:postDto.content,
+                createdAt:new Date(),
+                updatedAt:new Date(),
+                author:{
+                    connect:{
+                        id :postDto.authorId
+                    }
+                }
+            }
+        })
+        return data;
+    }catch (err){
+        console.log(err)
+        throw new Error("Something went wrong...")
+    }
+}
